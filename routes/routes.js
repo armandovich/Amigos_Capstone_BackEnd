1e06f9b3fa66ab944f63fca63361aa3a17bab2ca
Karim El Jazzar <jazzar.karim@gmail.com>
2022-12-14 17:53:37 -0500

    Fixed payment amount

--- routes/routes.js
+++ routes/routes.js
@@ -67,9 +67,12 @@ const SECRET_KEY = "sk_test_51MEhG7Igw91omEHPcgpk2YxPbVSQObGaQbCrkW8jq8wTZ7RAr6Z
 const stripe = Stripe(SECRET_KEY, { apiVersion: "2022-11-15" });
 
 router.post("/create-payment-intent", async (req, res) => {
+    const queryString = url.parse(req.url, true).query;
+
     try {
+      let amnt = queryString.amount
       const paymentIntent = await stripe.paymentIntents.create({
-        amount: 1099, //lowest denomination of particular currency
+        amount: amnt, //lowest denomination of particular currency
         currency: "usd",
         payment_method_types: ["card"], //by default
       });
